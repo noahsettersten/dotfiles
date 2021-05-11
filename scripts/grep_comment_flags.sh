6257632131945e68c3fc2cf91422783c49ba7f7f
Noah Settersten <noah@headway.io>
2021-05-11 08:13:21 -0500

    Exclude vendor / minified files from grep scripts

--- scripts/grep_comment_flags.sh
+++ scripts/grep_comment_flags.sh
@@ -2,7 +2,7 @@
 # Print out TODO, FIXME, DEBUG statements
 
 FILES_PATTERN='\.(js|ts|rb|erb|slim)(\..+)?$'
-FILES=$(git ls-files --cached | grep -E $FILES_PATTERN)
+FILES=$(git ls-files --cached -- . ':!:vendor/*' . ':!:*min.js' | grep -E $FILES_PATTERN)
 
 SEARCH='console.log|TODO|FIXME|DEBUG'
 echo "$FILES" | GREP_COLOR='4;5;37;41' xargs grep --color --with-filename -n -E $SEARCH
