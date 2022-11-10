bf8daf070e61b367100dff112eb5c0b769d01f91
Noah Settersten <noah@headway.io>
2022-11-10 17:37:07 -0600

    Update scripts

--- install.sh
+++ install.sh
@@ -1,34 +1,23 @@
 #!/bin/bash
 
-function symlink_files(){
-  shopt -s dotglob # Enable globbing over hidden files
-
-  local_dir=$1
-  destination_dir=$2
-
-  files="$local_dir/*"
-
-  for file_path in $files
-  do
-    file_name=$(basename $file_path)
-    ln -vnsF $file_path "$destination_dir/$file_name"
-  done
-
-  shopt -u dotglob # Disable globbing over hidden files
+function symlink_files() {
+  # Find all files in the .dotfiles/files directory and symlink to $HOME
+  find $1 -maxdepth 1 -iname ".*" -not -type d -exec cp -s {} $HOME \;
+  # find $1 -maxdepth 1 -iname ".*" -exec ln -vnsF {} $HOME \;
 }
 
-# get them home first
+# Start at home
 cd $HOME
 
+# Link general dotfiles into $HOME
 [ -d '.dotfiles' ] || git clone https://github.com/noahsettersten/dotfiles.git .dotfiles
 symlink_files "$(pwd)/.dotfiles/files" $HOME
 
-# mkdir $HOME/.vim
-# ln -vnsf $HOME/.dotfiles/config/plugins.vim $HOME/.vim
+# Link git template into $HOME
+ln -vnsf $HOME/.dotfiles/files/.git_template $HOME/.git_template
 
-# Neovim configuration folder
-# ln -vnsf $HOME/.dotfiles/nvim $HOME/.config/nvim
 
+# Link other config files into their respective locations
 mkdir $HOME/.newsboat
 ln -vnsf $HOME/.dotfiles/config/newsboat_config $HOME/.newsboat/config
 
@@ -37,8 +26,11 @@ ln -vnsf $HOME/.dotfiles/tmuxp $HOME/.tmuxp
 mkdir $HOME/.config
 ln -vnsf $HOME/.dotfiles/config/starship.toml $HOME/.config
 ln -vnsf $HOME/.dotfiles/config/fish/config.fish $HOME/.config/fish
+
 mkdir $HOME/.config/lvim
-ln -vnsf $HOME/.dotfiles/config/config.lua $HOME/.config/lvim/config.lua
+# ln -vnsf $HOME/.dotfiles/config/config.lua $HOME/.config/lvim/config.lua
+ln -vnsf $HOME/.dotfiles/lvim/lua $HOME/.config/lvim/lua
+
 mkdir $HOME/.config/alacritty
 ln -vnsf $HOME/.dotfiles/config/alacritty.yml $HOME/.config/alacritty/alacritty.yml
 
---
+++ scripts/nextcal.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+~/development/investment/nextcal/.build/debug/nextcal 2>/dev/null
