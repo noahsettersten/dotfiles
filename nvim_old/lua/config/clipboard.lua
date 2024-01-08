cd789c5b797c9dd0be97273382354ac596aa6799
Noah Settersten <noah@headway.io>
2024-01-08 13:09:57 -0600

    chore: Remove unused files

--- nvim_old/lua/config/clipboard.lua
+++
@@ -1,9 +0,0 @@
--- " Indent pastes properly
--- nnoremap <leader>pi p`[v`]=
-
--- " This allows you to share Vim's clipboard with OS X.
--- " It does prevent copy/paste in visual block mode though.
--- set clipboard=unnamed
-
--- " Copy current filename to clipboard
--- noremap <silent> <Leader>cf :let @+=expand("%")<CR>
--- nvim_old/lua/plugins/buffergator.lua
+++
@@ -1,5 +0,0 @@
--- let g:buffergator_viewport_split_policy="B"
--- let g:buffergator_split_size=10
--- let g:buffergator_suppress_keymaps=1
--- let g:buffergator_sort_regime = "mru"
--- map <Leader>b :BuffergatorToggle<cr>
