048c5ae8b59c6f32d278003bb2e78451cf3a4573
Noah Settersten <noah@headway.io>
2024-01-11 10:45:27 -0600

    chore: Clean up shortcuts.zsh

--- zsh/shortcuts.zsh
+++ zsh/shortcuts.zsh
@@ -4,21 +4,17 @@ unsetopt correct_all
 
 # System Level
 # ------------
-alias uldb='sudo /usr/libexec/locate.updatedb' #update the location database
-alias reindex='sudo mdutil -E /'
+# alias uldb='sudo /usr/libexec/locate.updatedb' #update the location database
+# alias reindex='sudo mdutil -E /'
 alias flush_dns='dscacheutil -flushcache'
-alias fixbrew='sudo chown -R `whoami` /usr/local'
 alias c='clear'
-alias cd..='cd ..'
 alias ..='cd ..'
-alias clera='clear'
-alias xit='exit'
+# alias xit='exit'
 alias fixcam='sudo killall VDCAssistant'
 alias hdu='du -d 1 -h | sort -h'
-alias myps='ps -ef | awk "/noah/ && !/fsevent_watch/ && !/Firefox/ && !/Dropbox/ && !/Spotify/ && !/1Password/ && !/CrashPlan/"'
 alias psfind="ps ax|grep -v grep|grep -iE"
-alias cpu='top -o cpu'
-alias mem='top -o rsize'
+# alias cpu='top -o cpu'
+# alias mem='top -o rsize'
 
 # Directories
 # alias l='ls -lahG'
@@ -26,7 +22,7 @@ alias l='lsd -lah'
 
 # Git
 # -----------------
-alias gg='git goggles'
+# alias gg='git goggles'
 alias gwc='git whatchanged --no-merges --pretty=format:"%Cgreen%h%Creset%x09%an%x09%Cblue%ar%Creset%x09%s"'
 alias gt='git tag'
 alias gtl='git tag -l'
@@ -35,9 +31,9 @@ alias gtlr='git ls-remote --tags'
 # alias gbnm='git branch --no-merged'
 alias gf='git fetch'
 alias glog='git log --oneline --graph'
-alias glog2="git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold yellow)%h%C(reset) %C(bold yellow)%d%C(reset) %C(white)%s%C(reset) - %C(bold green)(%ar)%C(reset)'"
-alias ptl='bundle exec rake pt:list'
-alias gmnff='git merge --no-edit --no-ff $*'
+# alias glog2="git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold yellow)%h%C(reset) %C(bold yellow)%d%C(reset) %C(white)%s%C(reset) - %C(bold green)(%ar)%C(reset)'"
+# alias ptl='bundle exec rake pt:list'
+# alias gmnff='git merge --no-edit --no-ff $*'
 alias gbc='git rev-parse --abbrev-ref HEAD 2>/dev/null | cut -d"/" -f 2 | tr -d "\n" | pbcopy' # copies the current branch to your clipboard (on os x)
 alias gp='git push;git push --tags'
 alias gs='git stash'
@@ -48,9 +44,9 @@ alias gsl='git stash list'
 alias gss='git stash show -p'
 alias grepflags='~/.dotfiles/scripts/grep_comment_flags.sh'
 alias grepflagsdiff='~/.dotfiles/scripts/grep_comment_flags_diff.sh'
-alias unstage='git reset HEAD --'
+# alias unstage='git reset HEAD --'
 alias uncommit='git reset --soft HEAD~1'
-alias recommit='git commit --amend --no-edit'
+# alias recommit='git commit --amend --no-edit'
 
 alias gbranch_changes='git diff --name-only main'
 
@@ -70,16 +66,6 @@ alias ta='tmux attach -t $*'
 # alias tk='tmux kill-session -t $*'
 # alias tks='tmux kill-server'
 
-# Wemux
-# -----
-alias wl='wemux list'
-alias wj='wemux join $*'
-alias wr='wemux rogue $*'
-alias wp='wemux pair $*'
-alias wsj='f() { wemux join $1; wemux start }; f'
-alias wk='wemux stop $*'
-alias wks='wemux stop'
-
 # Editors
 # ---
 alias nv='nvim'
@@ -87,18 +73,17 @@ alias v='nvim'
 # alias v='NVIM_APPNAME=nvim_astronvim nvim'
 # alias lv='lvim'
 alias slimvim='nvim -u NONE -U NONE -N $*'
-alias vim_solarized='nvim -c "set background=dark" -c "colorscheme solarized" $*'
 alias em='emacs -nw'
 
 # RAILS
 # -----
 alias b='bundle exec $*'
-alias rdm='bin/rake db:migrate'
+# alias rdm='bin/rake db:migrate'
 alias rc='bin/rails console'
 alias rdbc='bin/rails dbconsole'
-alias trc='RAILS_ENV=test bundle exec rails console'
-alias kr='kill -9 `cat tmp/pids/server.pid; echo;`'
-alias wph='bin/webpack-dev-server --hot --corejs 3.6.0 '
+# alias trc='RAILS_ENV=test bundle exec rails console'
+# alias kr='kill -9 `cat tmp/pids/server.pid; echo;`'
+# alias wph='bin/webpack-dev-server --hot --corejs 3.6.0 '
 alias tpd='tail ~/Library/Logs/puma-dev.log'
 
 # OTHER (ELIXIR / PHOENIX, NODE, ...)
@@ -107,9 +92,9 @@ alias phx='iex -S mix phx.server'
 alias fly_ip='fly dig -s aaaa $*'
 alias fly_livebook='ERL_AFLAGS="-proto_dist inet6_tcp" livebook server --name livebook@127.0.0.1'
 
-alias herokuiex='heroku run iex -S mix'
-alias nombomb='trash node_modules; bower cache clean && trash bower_components'
-alias storybook='BROWSER=none DISABLE_ESLINT_PLUGIN=true yarn storybook'
+# alias herokuiex='heroku run iex -S mix'
+# alias nombomb='trash node_modules; bower cache clean && trash bower_components'
+# alias storybook='BROWSER=none DISABLE_ESLINT_PLUGIN=true yarn storybook'
 alias webserver='python -m SimpleHTTPServer'
 
 alias pyactivate='source ./.env/bin/activate'
@@ -117,7 +102,7 @@ alias pydeactivate='source ./.env/bin/deactivate'
 
 # PAIR
 #-----
-unpair () { gh-auth remove --users=$1 }
+# unpair () { gh-auth remove --users=$1 }
 
 # VAGRANT
 # -------
@@ -131,20 +116,22 @@ alias vsus='vagrant suspend'
 alias prune_docker='docker system prune -a'
 alias docker_run_bash='docker run -it --entrypoint /bin/bash $*'
 alias docker_exec_bash='docker exec -it $1 /bin/bash'
+alias build_dots='docker build -t dotfiles:latest ~/.dotfiles'
+alias run_dots='docker run -it --entrypoint /bin/zsh dotfiles:latest'
 
 # PERSONAL TOOLS
 # --------------
-alias pipt='pip --trusted-host pypi.python.org'
+# alias pipt='pip --trusted-host pypi.python.org'
 alias showfiles='defaults write com.apple.finder AppleShowAllFiles YES'
 alias hidefiles='defaults write com.apple.finder AppleShowAllFiles NO'
 alias spotify_status='spotify status | grep -E "(Artist.*$|Track.*$)"'
 alias spotify_track='spotify status | sed -n "s/Track: //p"'
 # alias spotify_current_track='~/.dotfiles/scripts/spotify_current_track.sh'
 alias spt_track='spt pb -f "%s %a - %t"'
-alias slinvoke='serverless invoke local -f $*'
+# alias slinvoke='serverless invoke local -f $*'
 alias todos='ack --nobreak --nocolor "(TODO|FIXME):"|sed -E "s/(.*:[[:digit:]]+):.*((TODO|FIXME):.*)/\2 :>> \1/"|grep -E --color=always ":>>.*:\d+"'
 alias ql='qlmanage -px &>/dev/null'
-alias brewup='brew upgrade'
+# alias brewup='brew upgrade'
 alias ccanalyze='~/.dotfiles/scripts/codeclimate.sh'
 alias lsrubyversions='find . -name .ruby-version -exec bat {} \;'
 alias loc='cloc . --exclude-dir=node_modules'
@@ -169,5 +156,5 @@ brewdeps () {
 }
 
 # Edit/source this file
-alias ea='vim ~/.dotfiles/zsh/shortcuts.zsh'
+alias ea='hx ~/.dotfiles/zsh/shortcuts.zsh'
 alias ea_source='source $HOME/.dotfiles/zsh/shortcuts.zsh'
