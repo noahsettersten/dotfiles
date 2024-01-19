f54c2bc0ae239ebe3cf8e24ea38c16a9d877bf5b
Noah Settersten <noah@headway.io>
2024-01-19 10:20:18 -0600

    feat: Add calendar utility scripts

---
+++ scripts/agenda.sh
@@ -0,0 +1,8 @@
+# Writes out the list of events on the calendar for today
+#
+# Example output:
+# --Friday, January 19, 2024--
+#   11:30 AM-1:00PM Event name
+#     Location: web meeting link
+
+icalBuddy -f -sd -ss "" -df "%A, %B %e, %Y" -n -nc -iep "title,datetime,location" -ps "|: |\n  |" -po "datetime,title,location" eventsToday+10
---
+++ scripts/today.sh
@@ -0,0 +1,1 @@
+icalBuddy -f -sd -ss "" -n -nc -iep "title,datetime,location" -ps "|: |\n  |" -po "datetime,title,location" eventsToday
