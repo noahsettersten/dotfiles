7b2eb9efcf4f912bdf2ffce7e18c4b852c68e12b
Noah Settersten <noah@headway.io>
2024-03-29 15:58:38 -0500

    chore: Shorten agenda timeframe

--- scripts/agenda.sh
+++ scripts/agenda.sh
@@ -5,4 +5,4 @@
 #   11:30 AM-1:00PM Event name
 #     Location: web meeting link
 
-icalBuddy -f -sd -ss "" -df "%A, %B %e, %Y" -n -nc -iep "title,datetime,location" -ps "|: |\n  |" -po "datetime,title,location" eventsToday+10
+icalBuddy -f -sd -ss "" -df "%A, %B %e, %Y" -n -nc -iep "title,datetime,location" -ps "|: |\n  |" -po "datetime,title,location" eventsToday+7
