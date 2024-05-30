92bf2530b56652711869ba86c69616a2e3d13435
Noah Settersten <noah@headway.io>
2024-05-30 10:59:06 -0500

    chore: Update Trouble configuration

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -2,19 +2,26 @@
 return {
   {
     'folke/trouble.nvim',
-    branch = 'dev',
     keys = {
       {
         ' tr',
         '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
         mode = { 'n' },
-        desc = 'View LSP references',
+        desc = 'View LSP definitions / references',
       },
-      -- { ' tf', '<cmd>Trouble lsp_definitions<cr>', mode = { 'n' }, desc = 'View LSP definitions' },
-      { ' td', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', mode = { 'n' }, desc = 'View diagnostics' },
+      {
+        ' td',
+        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+        mode = { 'n' },
+        desc = 'View diagnostics',
+      },
+      { ' tw', '<cmd>Trouble diagnostics toggle<cr>', mode = { 'n' }, desc = 'View workspace diagnostics' },
+
       { ' tq', '<cmd>Trouble quickfix<cr>', mode = { 'n' }, desc = 'View quickfix' },
       { ' tl', '<cmd>Trouble loclist<cr>', mode = { 'n' }, desc = 'View loclist' },
-      { ' tw', '<cmd>Trouble diagnostics toggle<cr>', mode = { 'n' }, desc = 'View workspace diagnostics' },
+    },
+    opts = {
+      focus = true,
     },
   },
 }
