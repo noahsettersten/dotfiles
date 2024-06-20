13a75614fd16bfb164d6238d32967246bdf2ee67
Noah Settersten <noah@headway.io>
2024-06-20 14:43:43 -0500

    chore: Remove unused scripts

--- scripts/left_command.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-NEXT_EVENT=$($HOME/development/investment/nextcal/.build/debug/nextcal 2>/dev/null | head -n 1)
-UNREAD_COUNT=$($HOME/.dotfiles/scripts/unread_gmail.sh)
-
-echo "$UNREAD_COUNT Unread emails / $NEXT_EVENT"
--- scripts/nextcal.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-$HOME/development/investment/nextcal/.build/debug/nextcal 2>/dev/null | head -n 1
