89626fdefc6deb17e87ed46b78b421a252e308fa
Noah Settersten <noah@headway.io>
2025-03-01 08:52:31 -0600

    perf: Put email and liturgical calendar within bkt calls

--- scripts/liturgical_calendar.sh
+++ scripts/liturgical_calendar.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-LITURGICAL_CALENDAR=$(curl -s "http://calapi.inadiutorium.cz/api/v0/en/calendars/general-en/today")
+LITURGICAL_CALENDAR=$(bkt --ttl=8h --stale=45m -- curl -s "http://calapi.inadiutorium.cz/api/v0/en/calendars/general-en/today")
 
 WEEK=$(echo $LITURGICAL_CALENDAR | jq --raw-output '@text "Week \(.season_week) of \(.season) time"')
 # FEAST=$(echo $LITURGICAL_CALENDAR | jq --raw-output '@text "Celebrations: \(.celebrations[].title)"')
--- scripts/unread_gmail.sh
+++ scripts/unread_gmail.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-curl --connect-timeout 30 -s -u "$GMAIL_USERNAME":"$GMAIL_PASSWORD" "https://mail.google.com/mail/feed/atom" | \
+bkt --ttl=30m --stale=5m -- curl --connect-timeout 30 -s -u "$GMAIL_USERNAME":"$GMAIL_PASSWORD" "https://mail.google.com/mail/feed/atom" | \
   sed -n 's:.*<fullcount>\(.*\)</fullcount>.*:\1:p'
