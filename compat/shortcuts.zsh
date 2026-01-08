e283767643c3502e2c341a4c14a1d900ed9b3bb5
Noah Settersten <noah.settersten@planningcenter.com>
2026-01-08 08:37:52 -0600

    Refresh

--- compat/shortcuts.zsh
+++ compat/shortcuts.zsh
@@ -1,21 +1,13 @@
 # Nocorrect Aliases
-# -----------------
 unsetopt correct_all
 
-# System Level
-# ------------
+# -- Core --
 alias ..='cd ..'
-# alias hdu='du -d 1 -h | sort -h'
-# alias web='w3m https://duckduckgo.com'
-
-# Directories
 alias l='lsd -lah --date relative'
 alias d="cd $HOME/development/"
-alias dd="cd $HOME/development/dotfiles"
+# alias dd="cd $HOME/development/dotfiles"
 
-# Git
-# -----------------
-# Brought over from oh-my-zsh's git aliases
+# -- Git (Brought over from oh-my-zsh's git aliases) --
 alias ga='git add'
 alias gb='git branch'
 alias gc='git commit -v'
@@ -24,13 +16,11 @@ alias gcs='git checkout staging'
 alias gco='git checkout'
 alias gd='git diff'
 alias gst='git status'
-alias grbc='git rebase --continue'
-# End: Brought over from oh-my-zsh's git aliases
 
-# Other aliases?
+# -- Git (From other sources?) --
 alias gp='git push'
 alias gs='git stash'
-alias gsl='git stash list'
+# alias gsl='git stash list'
 alias gss='git stash show -p'
 alias uncommit='git reset --soft HEAD~1'
 
@@ -39,24 +29,14 @@ alias tl='tmux ls'
 alias ta='tmux attach -t $*'
 alias lg='lazygit'
 
+# -- Other utilities --
+alias hdu='du -d 1 -h | sort -h'
+alias web='w3m https://duckduckgo.com'
+
 # Frameworks
 alias b='bundle exec $*'
 alias rc='bin/rails console'
-# alias rdbc='bin/rails dbconsole'
-# alias btest='PARALLEL_WORKERS=4 bundle exec rails test'
-
-# PERSONAL TOOLS
-# mansearch () {
-#   man -k . | fzf --preview "echo {} | awk '{print $1}' | xargs -r man" | awk '{print $1}' | xargs -r man
-# }
-
-# brewdeps () {
-#   brew leaves | while read cask; do echo -n $fg[blue] $cask $fg[white]; brew deps $cask | awk '{printf(" %s ", $0)}'; echo ""; done
-# }
 
 # Edit/source this file
 # alias ea='hx ~/development/dotfiles/zsh/shortcuts.zsh'
 # alias ea_source='source $HOME/development/dotfiles/zsh/shortcuts.zsh'
-# alias claude="/Users/noah/.claude/local/claude"
-
-# alias dbg="devbox global"
--- shell/abbreviations.fish
+++ shell/abbreviations.fish
@@ -3,6 +3,8 @@ abbr -a .. cd ..
 abbr -a l lsd -lah --date relative
 abbr -a d cd $HOME/development/
 # abbr -a dd "cd $HOME/development/dotfiles"
+abbr -a hdu "du -d 1 -h | sort -h"
+abbr -a web w3m https://duckduckgo.com
 
 # -- Git (Brought over from oh-my-zsh's git aliases) --
 abbr -a ga git add
@@ -31,32 +33,25 @@ abbr -a tl tmux ls
 abbr -a ta tmux attach -t $argv
 abbr -a n "nnn -ce"
 abbr -a lg lazygit
-
-# -- Devbox --
-# abbr -a db devbox
 abbr -a outdated "brew outdated"
-
-# -- Other utilities --
-abbr -a hdu "du -d 1 -h | sort -h"
 abbr -a loc 'cloc --vcs git .'
-abbr -a web w3m https://duckduckgo.com
 
 # -- Ruby and Rails --
 abbr -a b bundle exec $argv
 abbr -a rc 'bin/rails console'
-abbr -a rdbc 'bin/rails dbconsole'
-# abbr -a localdb mysql -h 127.0.0.1 -u root
-abbr -a ldb mysql -h 127.0.0.1 -u root
+abbr -a ldb mycli -h 127.0.0.1 -u root
+abbr -a rt 'bin/rails test'
 
 # -- Personal --
-abbr -a bible $HOME/development/dotfiles/scripts/swiftbar_plugins/bible_in_a_year.1h.rb
+# abbr -a bible $HOME/development/dotfiles/scripts/swiftbar_plugins/bible_in_a_year.1h.rb
 # TODO: Retrieve Joshua Project's people group of the day (https://unreachedoftheday.org/)
-# history | LC_ALL=C sort | uniq -c | LC_ALL=C sort -nr | head -n 40 # TODO: Count commands in fish history to see how many times each abbreviation is used.
+# history | sort | uniq -c | sort -nr | head -n 40 # TODO: Count commands in fish history to see how many times each abbreviation is used.
 abbr -a compress_pdf gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dNOPAUSE -dQUIET -dBATCH -dPDFSETTINGS=/printer -sOutputFile=compressed.pdf
 
 # Temp
-abbr -a ts_count '~/development/dotfiles/scripts/ts_count.sh'
+# abbr -a ts_count '~/development/dotfiles/scripts/ts_count.sh'
 abbr -a test_changes "~/development/dotfiles/scripts/test_ruby_changes.rb"
 
 abbr -a tasks asana tasks list
-abbr -a projects asana projects list -l 25 --favorite
+# abbr -a projects asana projects list -l 25 --favorite
+abbr -a oc opencode
